130720f2843ed4b6b27090fa19e2d43fbb39c98b
Jacek Zalewski <jacekzalewski95@gmail.com>
2021-10-23 14:52:19 +0200

    Add CLI arguments parsing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,19 @@
 public class Main {
+
+	public static void main(String[] args) {
+		if (args.length == 1) {
+			// use custom configuration
+		}
+		
+		String host = args[0];
+		String jdbcPath = args[1];
+		String tableName = args[2];
+		String columns = args[3];
+		String pageSize, pageNumber;
+		if (args.length == 6) {
+			pageSize = args[4];
+			pageNumber = args[5];
+		}
+	}
+
 }
