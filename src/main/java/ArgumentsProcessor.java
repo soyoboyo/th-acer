1162ef6ddcc28d86da552e6414d1975e63e01f00
Jacek Zalewski <jacekzalewski95@gmail.com>
2021-10-23 16:15:11 +0200

    Add arguments processing ignoring order

---
+++ src/main/java/ArgumentsProcessor.java
@@ -0,0 +1,50 @@
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+public class ArgumentsProcessor {
+
+	private String host;
+	private String user;
+	private String password;
+	private String jdbcPath;
+	private String tableName;
+	private String columns;
+
+	public ArgumentsProcessor(String[] args) {
+		if (args.length == 1) {
+			// use custom configuration
+		}
+
+		Map<String, String> mappedArguments = mapArguments(args);
+
+		host = mappedArguments.get("--host");
+		user = mappedArguments.get("--user");
+		password = mappedArguments.get("--password");
+		jdbcPath = mappedArguments.get("--jdbcPath");
+		tableName = mappedArguments.get("--tableName");
+		columns = mappedArguments.get("--columns");
+		String pageSize = mappedArguments.get("--pageSize");
+		String pageNumber = mappedArguments.get("--pageNumber");
+	}
+
+	private Map<String, String> mapArguments(String[] args) {
+		Map<String, String> mappedAgs = new HashMap<>();
+		for (String arg : args) {
+			String[] params = arg.split("=");
+			if (params.length != 2) {
+				throw new IllegalArgumentException("Wrong settings for argument: " + arg);
+			}
+			String name = params[0];
+			if (!name.startsWith("--")) {
+				throw new IllegalArgumentException("Wrong format for argument: " + args);
+			}
+			String value = params[1];
+			mappedAgs.put(name, value);
+		}
+		return mappedAgs;
+	}
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,32 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Main {
 
 	public static void main(String[] args) {
-		if (args.length == 1) {
-			// use custom configuration
-		}
-
-		String host = args[0];
-		String jdbcPath = args[1];
-		String tableName = args[2];
-		String columns = args[3];
-		String pageSize, pageNumber;
-		if (args.length == 6) {
-			pageSize = args[4];
-			pageNumber = args[5];
-		}
 
 
-		try (Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "postgres");
-			 Statement stmt = conn.createStatement();
-			 ResultSet rs = stmt.executeQuery("SELECT * FROM employee;");) {
+		ArgumentsProcessor arguments = new ArgumentsProcessor(args);
+		String query = "SELECT " + arguments.getColumns() + " FROM " + arguments.getTableName() + ";";
 
+		try (Connection conn = DriverManager.getConnection
+				(arguments.getHost(),
+						arguments.getUser(),
+						arguments.getPassword());
+
+		) {
+			ResultSet rs = conn.createStatement().executeQuery(query);
 			while (rs.next()) {
 				System.out.print("ID: " + rs.getInt("id"));
-
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+
 		}
 	}
+
 
 }
